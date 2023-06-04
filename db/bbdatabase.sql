8814ea04897eb59a945b61d6ea980bb4cbe92dff
cjsalinas <47445339+xkaze09@users.noreply.github.com>
2023-06-05 06:52:28 +0800

    Add table structure for books

--- db/bbdatabase.sql
+++ db/bbdatabase.sql
@@ -13,3 +13,14 @@ CREATE TABLE admins (
     email VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL
 )  ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;;
+
+------table structure for books
+CREATE TABLE books (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  title VARCHAR(255) NOT NULL,
+  author VARCHAR(255) NOT NULL,
+  cover_image VARCHAR(255),
+  description TEXT,
+  genre VARCHAR(50),
+  availability BOOLEAN DEFAULT true
+);
